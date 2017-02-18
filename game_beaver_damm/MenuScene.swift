e1196c6dffb5f2f0cea3149b082e2c97d50e71d1
Minh Nguyen <minhnguyen_96@hotmail.com>
2017-02-18 15:40:10 +1000

    - instruction feature works with scrolling through images with specific image name prefix

--- game_beaver_damm/MenuScene.swift
+++ game_beaver_damm/MenuScene.swift
@@ -17,6 +17,7 @@ class MenuScene: SKScene {
     /* UI Label */
     var playLabel = SKLabelNode()
     var settingLabel = SKLabelNode()
+    var instructLabel = SKLabelNode()
     
     /* Mode UI Label */
     var easyLabel = SKLabelNode()
@@ -25,10 +26,12 @@ class MenuScene: SKScene {
     
     /* Layer */
     var buttonLayer = SKNode()
+    var instructionLayer = SKNode()
     
     /* Data */
     var difficulty: TimeInterval = Difficulty.easy
     var difficultyOptions: Array<SKNode> = []
+    var instructions: Instruction?
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder) is not used in this app")
@@ -38,7 +41,10 @@ class MenuScene: SKScene {
         /* Configure and add background */
         super.init(size: size)
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        buttonLayer.zPosition = 10
+        instructionLayer.zPosition = 20
         self.addChild(buttonLayer)
+        self.addChild(instructionLayer)
         
         difficultyOptions = [easyLabel, hardLabel]
         
@@ -54,8 +60,12 @@ class MenuScene: SKScene {
         let settingLblPos = CGPoint(x: 0, y: easyLabel.position.y - easyLabel.frame.height * 3)
         configureLabelNode(node: settingLabel, text: "SETTING", position: settingLblPos, layer: buttonLayer)
         
-        setDifficultyUI()
+        let instructLblPos = CGPoint(x: 0, y: settingLabel.position.y - settingLabel.frame.height * 3)
+        configureLabelNode(node: instructLabel, text: "INSTRUCTIONS", position: instructLblPos, layer: buttonLayer)
         
+        instructionLayer.isHidden = true
+        
+        setDifficultyUI()
     }
     
     func setDifficultyUI() {
@@ -71,15 +81,20 @@ class MenuScene: SKScene {
         node.fontName = "AvenirNext-Bold"
         node.position = position
         node.verticalAlignmentMode = .center
-        node.zPosition = 10
+        node.zPosition = buttonLayer.zPosition + 1
         layer.addChild(node)
     }
     
     // MARK: - touch functions
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first
-        let location = touch?.location(in: buttonLayer)
-        let touchedNode = buttonLayer.nodes(at: location!).first
+        let touch = touches.first! as UITouch
+        let location = touch.location(in: buttonLayer)
+        let touchedNode = buttonLayer.nodes(at: location).first
+        
+        if !instructionLayer.isHidden {
+            handleTouchOnInstructLayer(touches: touches)
+            return
+        }
         
         if touchedNode == playLabel {            
             let scene = GameScene(size: self.size, difficulty: difficulty)
@@ -89,11 +104,66 @@ class MenuScene: SKScene {
             scene.scaleMode = .aspectFill
             scene.size = skView.bounds.size
             skView.presentScene(scene)
- 
+            return
         }
         
         if touchedNode != nil && difficultyOptions.contains(touchedNode!) {
             setDifficulty(node: touchedNode!)
+            return
+        }
+        
+        if touchedNode == instructLabel {
+            handleTouchOnInstruct()
+            return
+        }
+        
+        
+    }
+    
+    // MARK: - Touch
+    
+    func handleTouchOnInstruct() {
+        instructions = Instruction()
+        instructionLayer.isHidden = false
+        
+        let prevBtn = SKSpriteNode(color: UIColor.green, size: CGSize(width: 20, height: 20))
+        let nextBtn = SKSpriteNode(color: UIColor.green, size: CGSize(width: 20, height: 20))
+        let exitBtn = SKLabelNode(text: "Exit")
+        
+        prevBtn.position = CGPoint(x: -self.size.width / 3, y: 0)
+        prevBtn.name = "PrevBtn"
+        prevBtn.zPosition = instructionLayer.zPosition
+        
+        nextBtn.position = CGPoint(x: self.size.width / 3, y: 0)
+        nextBtn.name = "NextBtn"
+        nextBtn.zPosition = instructionLayer.zPosition
+
+        
+        exitBtn.position = CGPoint(x: 0, y: -self.size.height / 4)
+        exitBtn.name = "Exit"
+        
+        instructionLayer.addChild(prevBtn)
+        instructionLayer.addChild(nextBtn)
+        instructionLayer.addChild(exitBtn)
+        instructionLayer.addChild((instructions?.currentImageNode)!)
+
+    }
+    
+    func handleTouchOnInstructLayer(touches: Set<UITouch>) {
+        let touch = touches.first! as UITouch
+        let location = touch.location(in: instructionLayer)
+        let touchedNode = instructionLayer.nodes(at: location).first
+        
+        if touchedNode == instructionLayer.childNode(withName: "PrevBtn") {
+            instructions?.setPrevTexture()
+        }
+        if touchedNode == instructionLayer.childNode(withName: "NextBtn") {
+            instructions?.setNextTexture()
+        }
+        
+        if touchedNode == instructionLayer.childNode(withName: "Exit") {
+            instructionLayer.removeAllChildren()
+            instructionLayer.isHidden = true
         }
     }
     
