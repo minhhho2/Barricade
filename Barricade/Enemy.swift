5a90b0e528e12d429ed4d38d4ad39a0c0355f03f
Minh Nguyen <minhnguyen_96@hotmail.com>
2017-02-22 18:26:42 +1000

    - fixing bad folder rename #2

---
+++ Barricade/Enemy.swift
@@ -0,0 +1,24 @@
+//
+//  Enemy.swift
+//  game_beaver_damm
+//
+//  Created by Minh Nguyen on 12/01/2017.
+//  Copyright © 2017 Minh Nguyen. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Enemy: Monster {
+    
+    var isAlive: Bool = true
+    var lastDirection: Direction = Direction.down
+    
+    init(col: Int, row: Int) {
+        super.init(col: col, row: row, imageName: Constants.CROC_SRITE)
+    }
+    
+
+    
+    
+}
---
+++ Barricade/Helper.swift
@@ -0,0 +1,67 @@
+//
+//  Helper.swift
+//  game_beaver_damm
+//
+//  Created by Minh Nguyen on 12/01/2017.
+//  Copyright © 2017 Minh Nguyen. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+enum Direction: String {
+    case none = "None", up = "Up", down = "Down", left = "Left", right = "Right"
+}
+
+struct Constants {
+    /* Initial Sprites */
+    static let playerSprite: String = "PlayerDown"
+    static let CROC_SRITE: String = "Croc"
+    static let BLOCK_SPRITE: String = "Block"
+}
+
+struct Message {
+    static let newGame = "Tap To Play"
+}
+struct Game {
+    // Odd number for pretty board
+    static let numRows: Int = 13
+    static let numCols: Int = 13 // HAS TO BE BIGGER THAN GAP
+    static let initialStage: Int = 1
+}
+
+struct Constraint {
+    static let colGap = 3
+    static let rowGap = 3
+}
+
+struct AnimationTime {
+    static let player = 0.25
+    static let block = 0.25
+    static var enemy = 0.5
+}
+
+/* Tuple that represents the column and row on the board */
+struct BoardPosition {
+    let col: Int
+    let row: Int
+}
+
+struct Difficulty {
+    static let easy: TimeInterval = 1.0
+    static let hard: TimeInterval = 0.50
+}
+
+struct LayerZPos {
+    static let gameLayerZ: CGFloat = 10
+    static let tileLayerZ: CGFloat = 20
+    static let objectLayerZ: CGFloat = 30
+    static let touchableLayerZ: CGFloat = 40
+    static let menuLayerZ: CGFloat = 50
+    static let pauseLayerZ: CGFloat = 50
+    static let gameOverLayerZ: CGFloat = 50
+    static let settingLayerZ: CGFloat = 50
+    
+    static let instructionLayerZ: CGFloat = 20
+    static let buttonLayerZ: CGFloat = 10
+}
