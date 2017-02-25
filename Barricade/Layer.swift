c8703821cce78941153332e8978fd1aeb225d564
Minh Nguyen <minhnguyen_96@hotmail.com>
2017-02-25 21:51:58 +1000

    - fixed attempt #2 for github

---
+++ Barricade/Layer.swift
@@ -0,0 +1,41 @@
+//
+//  Layer.swift
+//  game_beaver_damm
+//
+//  Created by Minh Nguyen on 19/02/2017.
+//  Copyright © 2017 Minh Nguyen. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Layer {
+    init() {
+        
+    }
+    
+    func checkIsHidden() {
+        
+    }
+    
+    func checkIsShown() {
+        
+    }
+    
+    func showLayer() {
+        
+    }
+    
+    func hideLayer() {
+        
+    }
+    
+    func addLayerBackground() {
+        
+    }
+    
+    func removeLayerBackground() {
+        
+    }
+    
+}
---
+++ Barricade/Tile.swift
@@ -0,0 +1,15 @@
+//
+//  Tile.swift
+//  game_beaver_damm
+//
+//  Created by Minh Nguyen on 12/01/2017.
+//  Copyright © 2017 Minh Nguyen. All rights reserved.
+//
+
+import Foundation
+
+class Tile {
+    init() {
+        
+    }
+}
